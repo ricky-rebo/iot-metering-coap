16874e608579b599b2dc963cb7ca240015cea0b4
Riccardo Rebottini <ricky@rebottini.it>
2022-02-22 11:56:16 +0100

    :alembic: testing system...

--- src/main/java/it/unimore/dipi/iot/metering/client/CoapGetClientProcess.java
+++ src/main/java/it/unimore/dipi/iot/metering/client/CoapGetClientProcess.java
@@ -17,8 +17,9 @@ public class CoapGetClientProcess {
 
 	private final static Logger logger = LoggerFactory.getLogger(CoapGetClientProcess.class);
 
-	private static final String COAP_ENDPOINT = "coap://127.0.0.1:5683/meters/3f42c08d-0622-4094-bdb1-d6bc5ad2e4f4/state";
+//	private static final String COAP_ENDPOINT = "coap://127.0.0.1:5683/meters/3f42c08d-0622-4094-bdb1-d6bc5ad2e4f4/state";
 //	private static final String COAP_ENDPOINT = "coap://127.0.0.1:5684/.well-known/core";
+	private static final String COAP_ENDPOINT = "coap://127.0.0.1:5684/switch";
 
 	public static void main(String[] args) {
 		
@@ -28,7 +29,7 @@ public class CoapGetClientProcess {
 		//Request Class is a generic CoAP message: in this case we want a GET.
 		//"Message ID", "Token" and other header's fields can be set 
 		Request request = new Request(Code.POST);
-//		request.setOptions(new OptionSet().setAccept(MediaTypeRegistry.APPLICATION_SENML_JSON));
+		request.setOptions(new OptionSet().setAccept(MediaTypeRegistry.APPLICATION_SENML_JSON));
 //		request.setOptions(new OptionSet().setAccept(MediaTypeRegistry.APPLICATION_LINK_FORMAT));
 
 		//Set Request as Confirmable
@@ -41,7 +42,8 @@ public class CoapGetClientProcess {
 
 			String text = coapResp.getResponseText();
 			logger.info("--------------------------------------------------------");
-			logger.info("Payload: \n{}", text.replace(",", "\n"));
+			logger.info("Response code: {}", coapResp.getCode());
+			logger.info("Payload: \n{}", text/*.replace(",", "\n")*/);
 			logger.info("Message ID: " + coapResp.advanced().getMID());
 			logger.info("Token: " + coapResp.advanced().getTokenString());
 			logger.info("--------------------------------------------------------");
